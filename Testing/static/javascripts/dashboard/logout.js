5ba5317d4aa016daea14057e70b6fe1c7d233736
tayawaaean <143526706+tayawaaean@users.noreply.github.com>
2024-02-29 13:29:51 +0800

    Delete Testing/static directory

--- Testing/static/javascripts/dashboard/logout.js
+++
@@ -1,9 +0,0 @@
-function confirmLogout() {
-                // Use the built-in confirm function to show a confirmation dialog
-                var confirmLogout = confirm("Are you sure you want to log out?");
-                
-                // If the user confirms, redirect to the login page
-                if (confirmLogout) {
-                    window.location.href = "login.html";
-                }
-            }
